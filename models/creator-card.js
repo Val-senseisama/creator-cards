39c8a1aab35bed088093ff66a82b5d233cbd06a9
David Val <valizevbigiedavid@gmail.com>
2026-06-23 23:47:03 +0100

    feat: add CreatorCard Mongoose model
    Defines the creator_cards collection schema with ULID _id, unique slug,
    paranoid soft-delete support, and all fields required by the spec.

---
+++ models/creator-card.js
@@ -0,0 +1,38 @@
+const { ModelSchema, SchemaTypes, DatabaseModel } = require('@app-core/mongoose');
+
+const modelName = 'creator_cards';
+
+/**
+ * @typedef {Object} ModelSchema
+ * @property {String} _id
+ * @property {String} title
+ * @property {String} description
+ * @property {String} slug
+ * @property {String} creator_reference
+ * @property {Array} links
+ * @property {Object} service_rates
+ * @property {String} status
+ * @property {String} access_type
+ * @property {String} access_code
+ * @property {Number} created
+ * @property {Number} updated
+ * @property {Number} deleted
+ */
+
+const schemaConfig = {
+  _id: { type: SchemaTypes.ULID },
+  title: { type: SchemaTypes.String },
+  description: { type: SchemaTypes.String },
+  slug: { type: SchemaTypes.String, unique: true, index: true },
+  creator_reference: { type: SchemaTypes.String, index: true },
+  links: { type: SchemaTypes.Array },
+  service_rates: { type: SchemaTypes.Mixed },
+  status: { type: SchemaTypes.String, index: true },
+  access_type: { type: SchemaTypes.String, default: 'public' },
+  access_code: { type: SchemaTypes.String },
+};
+
+const modelSchema = new ModelSchema(schemaConfig, { collection: modelName });
+
+/** @type {ModelSchema} */
+module.exports = DatabaseModel.model(modelName, modelSchema, { paranoid: true });
